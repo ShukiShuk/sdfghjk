84eba9cd490f85d16496b26f56649c271e44761d
ShukiShuk <47628097+ShukiShuk@users.noreply.github.com>
2019-02-14 11:45:00 +0200

    whatever
    fjkfjeklrs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Shiran!</h1>
+<h1>Hello Shiran222!</h1>
 <div>
   We use Parcel to bundle this sandbox, can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
